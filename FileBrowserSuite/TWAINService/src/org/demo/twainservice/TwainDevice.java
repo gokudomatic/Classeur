9ec0285a5a9da7a7d46dd22223e568510befb052
gokudomatic <gourry.gabrief@gmail.com>
2011-03-21 18:25:37 +0100

    implemented twain connector

--- FileBrowserSuite/TWAINService/src/org/demo/twainservice/TwainDevice.java
+++ FileBrowserSuite/TWAINService/src/org/demo/twainservice/TwainDevice.java
@@ -2,32 +2,65 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.demo.twainservice;
 
+import java.awt.image.BufferedImage;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.event.EventListenerList;
 import org.demo.scannerservice.Parameters;
 import org.demo.scannerservice.ScannerDevice;
 import org.demo.scannerservice.ScannerListener;
+import uk.co.mmscomputing.device.scanner.Scanner;
+import uk.co.mmscomputing.device.scanner.ScannerIOException;
+import uk.co.mmscomputing.device.scanner.ScannerIOMetadata;
+import uk.co.mmscomputing.device.scanner.ScannerIOMetadata.Type;
 
 /**
  *
  * @author edwin
  */
-public class TwainDevice implements ScannerDevice{
+public class TwainDevice implements ScannerDevice, uk.co.mmscomputing.device.scanner.ScannerListener {
+
+    private static Scanner scanner = null;
+    private final EventListenerList listeners = new EventListenerList();
 
     @Override
     public void addListener(ScannerListener listener) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        listeners.add(ScannerListener.class, listener);
     }
 
     @Override
     public void removeListener(ScannerListener listener) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        listeners.remove(ScannerListener.class, listener);
     }
 
     @Override
     public void acquire() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Thread t = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                if (scanner == null) {
+                    scanner = Scanner.getDevice();
+                    scanner.addListener(TwainDevice.this);
+                }
+                try {
+                    scanner.acquire();
+                } catch (ScannerIOException ex) {
+                    Logger.getLogger(TwainDevice.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+            }
+        });
+        t.start();
+    }
+
+    protected void fireScanPerformed(BufferedImage img) {
+        final ScannerListener[] list = listeners.getListeners(ScannerListener.class);
+        for (ScannerListener l : list) {
+            l.imageAcquired(img);
+        }
     }
 
     @Override
@@ -40,4 +73,19 @@ public class TwainDevice implements ScannerDevice{
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
+    public void update(Type type, ScannerIOMetadata metadata) {
+        try {
+            if (type.equals(ScannerIOMetadata.ACQUIRED)) {
+                BufferedImage image = metadata.getImage();
+                fireScanPerformed(image);
+            } else if (type.equals(ScannerIOMetadata.STATECHANGE)) {
+                System.out.println(metadata.getStateStr());
+            } else if (type.equals(ScannerIOMetadata.EXCEPTION)) {
+                metadata.getException().printStackTrace();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
